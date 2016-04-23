f3a8ee79fb13094c4f06fcd1e4a25c4967d07ff3
Ekaterina <zagirovaea@mail.ru>
2016-04-23 11:15:27 +0500

    end of app.py

--- app.py
+++ app.py
@@ -6,27 +6,32 @@ def main(file_name):
     print('Данная функция выводит максимальное значение для цены закрытия по месяцам.')
     #input(AMD.csv)
     with open(file_name) as f:
+        f.readline()
         csv_file = csv.reader(f, delimiter=',')
-        flag = 0
+        flag2 = 0
         maximum = -1
         month = -1
         print("Месяц   MAX значение")
         for row in csv_file:
-            if flag == 0:
-                flag = 1
-                continue
             num = float(row[4])
             data = row[0].split('-')
             if month == int(data[1]):
                 if maximum < num:
                     maximum = num
+            elif flag2 == 1:
+                print(repr(month).rjust(4), repr(maximum).rjust(12), end='\n')
+                #print(' {0:5d} {1:5d} '.format(month, maximum))
+                #print(month, '   ', maximum)
+                month = int(data[1])
+                maximum = num
             else:
-                #print(' {0:4d} {1:10d} '.format(month, maximum))
-                print(month, '   ', maximum, '\n')
+                flag2 = 1
                 month = int(data[1])
                 maximum = num
             #massiv.append(float(row[4]))
             #print('\n'.join(float(row[4])))
+        print(repr(month).rjust(4), repr(maximum).rjust(12), end='\n')
+        #print(month, '   ', maximum)
 
 
 if __name__ == "__main__":
