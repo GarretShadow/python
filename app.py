c4b9b276e4a937e708e62f477384afdb8997c1c7
Ekaterina <zagirovaea@mail.ru>
2016-06-07 21:56:04 +0500

    kursovaya 1 step

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import datetime
 import logging
 import sys
 
-# import time
 from process import write_file, process_file, process_network
+from indicator import indicator_read
 
 if __name__ == "__main__":
     # проверка аргументов
@@ -16,6 +16,7 @@ if __name__ == "__main__":
     parser.add_argument("-s", "--save", help="сохранит результат в файл")
     parser.add_argument("-lf", "--logfile", help="определяет имя лог файла")
     parser.add_argument("-l", "--log", help="уровень логирования")
+    parser.add_argument("-i", "--indicator", help="считает Стохасти́ческий осциллятор")
     # parser.add_argument("-t", "--time", choices=["n", "y"], help="позволяет определить период данных")
     args = parser.parse_args()
 
@@ -24,30 +25,27 @@ if __name__ == "__main__":
         level = getattr(logging, args.log.upper(), None)
         logging.basicConfig(filename='app.log', level=level)
     except:
-        print("Unexpected error:", sys.exc_info()[0])
+        logging.error("Unexpected error:", sys.exc_info()[0])
     logging.info('Программа запущена')  # Вывод информационных соообщений
 
     # инициализация дат
     start, end = datetime.date(2015, 1, 1), datetime.date(2015, 12, 31)
-    # try:
-    #     if args.time == "y":
-    #         print("Введите начало и конец периода наблюдения:")
-    #         #считать start, end = datetime.date(args.start_t), datetime.date(args.end_t)
-    # except:
-    #     print("Неправильный формат дат")
-    #     logging.error("Неправильный формат дат")
 
-    print('Данная программа выводит максимальное значение для цены закрытия по месяцам.')
-    if args.file:
-        process_file(args.file, start, end)  # функция выполняет обработку файла
-    elif args.symbol:
-        process_network(args.symbol, start, end)  # читать данные по сети
+    #print('Данная программа выводит максимальное значение для цены закрытия по месяцам.')
+    # if args.file:
+    #     process_file(args.file, start, end)  # функция выполняет обработку файла
+    # elif args.symbol:
+    #     process_network(args.symbol, start, end)  # читать данные по сети
+
+
+    indicator_read(args.file)
+
 
     # запись в файл
-    try:
-        write_file('out.txt')
-    except:
-        print('')
-    # if (args.save() is not None):
-    #     writeFile('out_app.txt')
+    # try:
+    #     if args.save() != "":
+    #         write_file('out.txt')
+    # except:
+    #     logging.error("Не удалось записать в файл")
+
 
---
+++ indicator.py
@@ -0,0 +1,93 @@
+import csv
+import logging
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Стохасти́ческий осциллятор (стоха́стик, стоха́стика от англ. stochastic oscillator) —
+# индикатор технического анализа, который показывает положение текущей цены относительно диапазона цен
+# за определенный период в прошлом. Измеряется в процентах.
+#
+# K - быстрый стохастик (сплошная линия, основной график)
+# K(t) = (C(t) - Ln) / (Hn - Ln) * 100
+# C - цена закрытия текущего периода
+# Ln - самая низкая цена за последние n периодов
+# Hn - самая высокая цена за последние n периодов
+#
+# D - медленный стохастик (пунктирная линия, дополнительно усреднённый график)
+# D является скользящей средней относительно K с небольшим периодом усреднения.
+
+
+def stochastic_oscillator (data: list, window: int):
+    K, upper_line, bottom_line = [], [], []
+    d = data[1][0] - data[1][1]
+    N = range(len(data[0]))
+    for t in N:
+        K.append((data[0][t] - data[1][1]) / d * 100)
+    D = moving_average(K, window, 'exp')
+    #del(K[0:window])
+    N = range(len(K))
+    for t in N:
+        upper_line.append(80.0)
+        bottom_line.append(20.0)
+    trading_signals(K, D, data[2])
+    plt.plot(N, K, 'r', N, D, 'b--', N, upper_line, 'k--', N, bottom_line, 'k--')
+    plt.legend(['K(t)', 'D(t)', '80%', '20%'],
+               loc='upper right')
+    plt.show()
+
+
+def moving_average(data: list, window: int, type='simple'):
+    # compute an n period moving average.
+    # type is 'simple' | 'exponential'
+    data = np.asarray(data)
+    if type == 'simple':
+        weights = np.ones(window)
+    else:
+        weights = np.exp(np.linspace(-1., 0., window))
+
+    weights /= weights.sum()
+
+    res = np.convolve(data, weights, mode='full')[:len(data)]
+    res[:window] = res[window]
+    return res
+
+def trading_signals (K: list, D: list, time: list):
+    #покупать - K, D СТАЛИ выше 20%, K выше D
+    #продавать - K, D СТАЛА ниже 80%, K ниже D
+    N = range(1, len(K))
+    table = {}
+    for t in N:
+        if (K[t] > D[t]) or (K[t] > 20.0 and K[t-1] < 20.0) or (D[t] > 20.0 and D[t-1] < 20.0):
+            table[time[t]] = 'BUY'
+        elif (K[t] < D[t]) or (K[t] < 80.0 and K[t-1] > 80.0) or (D[t] < 80.0 and D[t-1] > 80.0):
+            table[time[t]] = 'SELL'
+    indicator_write(table)
+
+def indicator_read(file_name: str):
+    try:
+        f = open(file_name, 'r')
+    except IOError:
+        print('Не может открыть файл ', file_name)
+        logging.error("Не может открыть файл", file_name)
+        return
+    #data = [[C(t)], [Hn, Ln]]
+    data = [[], [0.0, 9999999.9], []]
+    f.readline()
+    for row in csv.reader(f, delimiter=','):
+        data[0].append(float(row[4]))
+        data[2].append(row[0])
+        if data[1][0] < float(row[2]):
+            data[1][0] = float(row[2])
+        if data[1][1] > float(row[3]):
+            data[1][1] = float(row[3])
+    stochastic_oscillator(data, 6)
+
+def indicator_write(data):
+    f = open("indicator.txt", 'w')
+    f.write('{0:10} {1:10}'.format('Дата', 'Торговый сигнал') + '\n')
+    for k, v in sorted(data.items()):
+        f.write('{0:10} {1:10}'.format(k, v) + '\n')
+    f.close()
+
+
